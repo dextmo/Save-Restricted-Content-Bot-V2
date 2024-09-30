6041a8ad259ac4ed1283ba4ccea3eefc76f3250f
dextmo <m6649964@gmail.com>
2024-09-30 12:02:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "21727017"))
+API_HASH = getenv("API_HASH", "8c9ff17d8d1f6fbe228e5133b011fa99")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7523868885").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://dexmoer:dex23@dex.nm2sl.mongodb.net/?retryWrites=true&w=majority&appName=dex")
+LOG_GROUP = getenv("LOG_GROUP", "-1002343059720")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002343059720"))
